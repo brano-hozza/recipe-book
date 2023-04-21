376601a1b10e9b0ee18c23ad157a9af57afefa72
Branislav Hozza <brankohozza@gmail.com>
2023-04-21 15:04:01 +0200

    :wastebasket: cleanup & add sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is an welcome page</h1>
   </main>
 </template>
+<style lang="scss" scoped>
+@media (min-width: 1024px) {
+  main {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
