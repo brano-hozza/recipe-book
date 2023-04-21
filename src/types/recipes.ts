a359907d4f4b36cf441ee6692986c2990b6f9e19
Branislav Hozza <brankohozza@gmail.com>
2023-04-21 17:12:14 +0200

    :sparkles: display some resources

--- src/types/recipes.ts
+++ src/types/recipes.ts
@@ -1,25 +1,25 @@
 export interface Recipe {
   id: number
   name: string
-  img: string
-  description: string
-  ingredients: string[]
+  img?: string
+  description?: string
+  ingredients: number[]
   instructions: string[]
 }
 
 export interface NewRecipeDTO {
   name: string
-  img: string
-  description: string
-  ingredients: string[]
+  img?: string
+  description?: string
+  ingredients: number[]
   instructions: string[]
 }
 
 export interface UpdateRecipeDTO {
   id: number
   name: string
-  img: string
-  description: string
-  ingredients: string[]
+  img?: string
+  description?: string
+  ingredients: number[]
   instructions: string[]
 }
--- src/types/resources.ts
+++ src/types/resources.ts
@@ -2,21 +2,22 @@ export interface Resource {
   id: number
   name: string
   category: string
-  description: string
-  img: string
+  description?: string
+  img?: string
+  quantity: number
 }
 
 export interface NewResourceDTO {
   name: string
   category: string
-  description: string
-  img: string
+  description?: string
+  img?: string
 }
 
 export interface UpdateResourceDTO {
   id: number
   name: string
   category: string
-  description: string
-  img: string
+  description?: string
+  img?: string
 }
